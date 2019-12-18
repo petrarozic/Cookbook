dffdef9020baaa91edcd7fe284fd4581f663a88d
unknown <petra.rozic01@gmail.com>
2019-12-18 13:04:18 +0100

    Added simple tests

---
+++ Cookbook/Cookbook.HighLevelTests/Class1.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+using Xunit;
+
+namespace Cookbook.HighLevelTests
+{
+    public class Class1 : IDisposable
+    {
+        private readonly IWebDriver _driver;
+
+        public Class1()
+        {
+            _driver = new ChromeDriver(Environment.CurrentDirectory);
+        }
+        public void Dispose()
+        {
+            _driver.Quit();
+            _driver.Dispose();
+        }
+
+        [Fact]
+        public void getContent()
+        {
+            _driver.Navigate().GoToUrl("http://localhost:58883/");
+            DelayForDemoVideo(1000);
+            
+            Assert.Contains("Hello World!", _driver.PageSource.ToString());
+        }
+
+        private void DelayForDemoVideo(int delay = 500)
+        {
+            Thread.Sleep(delay);
+        }
+    }
+}
--- Cookbook/Cookbook/Startup.cs
+++ Cookbook/Cookbook/Startup.cs
@@ -15,14 +15,16 @@ namespace Cookbook
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            //if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseMvcWithDefaultRoute();
             }
 
             app.Run(async (context) =>
